5e24c49700db7649ab87049428c2cef926e5b6ef
Junior Salvador <junioronliner@gmail.com>
2024-10-19 12:43:06 -0300

    feat: added DNS service, Service Discovery and internal LB :rocket:

--- output.tf
+++ output.tf
@@ -2,10 +2,19 @@ output "load_balancer_dns" {
   value = aws_lb.main.dns_name
 }
 
+output "internal_load_balancer_dns" {
+  value = aws_lb.internal.dns_name
+}
+
 output "lb_ssm_arn" {
   value = aws_ssm_parameter.lb_arn.id
 }
 
 output "lb_ssm_listener" {
   value = aws_ssm_parameter.lb_listener.id
+}
+
+output "cloudmap_ssm" {
+  description = "ID do Namespace de Service Discovery do Cluster ECS"
+  value = aws_ssm_parameter.cloudmap.id
 }
